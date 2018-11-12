ac7ea99055c9f3f8da2ba7f5b614b6752cc5adfa
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-11-12 16:59:53 -0600

    finish mini exercise

--- src/BurgerTester.java
+++ src/BurgerTester.java
@@ -1,14 +1,15 @@
 public class BurgerTester {
     public static void main(String[] args) {
-        BurgerTools B = new BurgerTools();
-        BurgerTools C = new BurgerTools();
-        System.out.println(B.averageDaysBeforeExpiration);
-        B.averageDaysBeforeExpiration = 6;
-        System.out.println(C.averageDaysBeforeExpiration);
-        B.grill();
-        System.out.println(B.currentTempurature);
-        System.out.println(BurgerTools.averageDaysBeforeExpiration);
+        BurgerTools B = new BurgerTools("dhfjksha",10, 50);
+//        System.out.println(B.averageDaysBeforeExpiration);
+//        B.averageDaysBeforeExpiration = 6;
+//        System.out.println(C.averageDaysBeforeExpiration);
+//        B.grill();
+//        System.out.println(B.currentTempurature);
+//        System.out.println(BurgerTools.averageDaysBeforeExpiration);
 //        System.out.println(BurgerTools.currentTempurature); //cannot execute
+        B.setCurrentTempurature(200);
+        System.out.println(B.getCurrentTempurature());
 
     }
 }
--- src/BurgerTools.java
+++ src/BurgerTools.java
@@ -9,12 +9,56 @@ public class BurgerTools {
 //            and testing methods.
 //
 //    If you have time, add additional properties and methods.
-    public String mostPopularTopping;
-    public static int averageDaysBeforeExpiration = 5;
-    public int temperatureWhenCooked = 100;
-    public int currentTempurature;
-    public void grill(){
+
+//    -- SHORT ASSIGNMENT #2 (part 2) --
+//    Refactor your RestaurantDish and RestaurantTest classes...
+// 1) Change your public properties in your RestaurantDish class to private
+// 2) Add getters and setters for these properties (write them manually then erase them and use IntelliJ to do this quickly)
+// 3) Refactor code used in RestaurantTest to set and print these property values...
+//            ...to use your getters and setters
+// 4) Add a constructor method to RestaurantDish that will assign property...
+//            ...values based on arguments passed into the constructor
+
+    private String mostPopularTopping;
+    private static int averageDaysBeforeExpiration = 5;
+    private int temperatureWhenCooked = 100;
+    private int currentTempurature = 70;
+    private void grill(){
         currentTempurature = 150;
         System.out.println("Grilling burger");
+    }
+
+// CONSTRUCTOR
+    public BurgerTools(String mostPopularTopping, int temperatureWhenCooked, int currentTempurature) {
+        this.mostPopularTopping = mostPopularTopping;
+        this.temperatureWhenCooked = temperatureWhenCooked;
+        this.currentTempurature = currentTempurature;
+    }
+
+// GETTERS
+    public String getMostPopularTopping() {
+        return mostPopularTopping;
+    }
+    public static int getAverageDaysBeforeExpiration() {
+        return averageDaysBeforeExpiration;
+    }
+    public int getTemperatureWhenCooked() {
+        return temperatureWhenCooked;
+    }
+    public int getCurrentTempurature() {
+        return currentTempurature;
+    }
+// SETTER
+    public void setMostPopularTopping(String mostPopularTopping) {
+        this.mostPopularTopping = mostPopularTopping;
+    }
+    public static void setAverageDaysBeforeExpiration(int averageDaysBeforeExpiration) {
+        BurgerTools.averageDaysBeforeExpiration = averageDaysBeforeExpiration;
+    }
+    public void setTemperatureWhenCooked(int temperatureWhenCooked) {
+        this.temperatureWhenCooked = temperatureWhenCooked;
+    }
+    public void setCurrentTempurature(int currentTempurature) {
+        this.currentTempurature = currentTempurature;
     }
 }
