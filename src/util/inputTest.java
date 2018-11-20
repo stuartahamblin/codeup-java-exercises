3583198fdb7fa0d068b49d0d2ce224f83dd31d8c
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-11-20 15:56:31 -0600

    exceptions exercise complete

--- src/util/inputTest.java
+++ src/util/inputTest.java
@@ -5,10 +5,19 @@ public class inputTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Input in = new Input(sc);
-        System.out.println(in.getString());
-        System.out.println(in.yesNo());
+//        System.out.println(in.getString());
+//        System.out.println(in.yesNo());
 //        System.out.println(in.getInt(10,16));
 //        System.out.println(in.getDouble(.005, .1000));
 
+//        System.out.println(in.getInt());
+//        System.out.println(in.getDouble());
+//        Integer.valueOf(3);
+//        try {
+//            Double.valueOf("4.5");
+//        } catch(NumberFormatException e){
+//            System.out.println("this is a double");
+//        }
+        System.out.println(in.getDouble(10,15));
     }
 }
